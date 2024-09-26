f5f065d80d287293fd70d2182d8f3a18dd9c2103
superfreak58 <rayjkolyn@gmail.com>
2024-09-26 08:51:23 -0400

    sigma

--- Program.cs
+++ Program.cs
@@ -35,11 +35,15 @@
             }
             System.Threading.Thread.Sleep(1000);
             Console.Clear();
-            double bankBal, depoNum;
+
+            double bankBal, depoNum, withNum;
+            
+            bankBal = 150;
+            done = false;
             Console.WriteLine("Welcome to the Bank Of Blorb ATM what would you like to do?");
             string choice = "";
 
-            while (choice != "q")
+            while (!done)
             {
 
 
@@ -57,24 +61,25 @@
                 {
 
                     Console.WriteLine("You picked Deposit! how much are you depositing");
-                    depoNum = Convert.ToInt32(Console.ReadLine();
+                    depoNum = Convert.ToInt32(Console.ReadLine());
+                    bankBal += depoNum;
 
                 }
                 else if (choice == "2")
                 {
 
-                    Console.WriteLine("You chose option 2");
-                    Console.WriteLine("Hit ENTER to continue.");
-                    Console.ReadLine();
+                    Console.WriteLine("You chose Withdraw! how much are you taking out?");
+                    withNum = Convert.ToInt32(Console.ReadLine());
+                    bankBal -= withNum;
                 }
                 else if (choice == "3") 
-                { 
-                
+                {
+                    Console.WriteLine("Your current balance is $" + bankBal);
                 }
                 else if (choice == "4") 
                 { 
                 Console.WriteLine("Thank you for using BOB!");
-                   done= true;
+                    done = true;    
                 }
                 else
                 {
